d06b3fd09e9224809595747c78262580ef5de256
Antoine Dabin <antoine.dabin@epitech.eu>
2025-01-21 02:40:14 +0100

    fix: Change languages list to single strings / changed help function to display possibles options arguments

--- header.sh
+++ header.sh
@@ -3,8 +3,8 @@
 # Constants
 VALID_LANGUAGES=("fr" "en" "de" "es" "it")
 VALID_COMMENTS=("#" "//" ";" "/")
-VALID_SHEBANGS=("bash" "python" "perl" "ruby" "php")
-VALID_TEMPLATES=("default" "custom")
+VALID_SHEBANGS=("sh" "py" "pl" "rb" "php" "go")
+VALID_TEMPLATES=("default" "single")
 
 # Variables
 current_dir_path=${PWD##*/}
@@ -14,18 +14,15 @@ user_name=${user_name:-"unknown"}
 version="1.0.0"
 
 # Options Variables
-character="#"
 lang="en"
 language="bash"
-comment_character="#"
 shebang="none"
 template="default"
 
 dest_file=${@: -1}
 
 # Translations
-declare -A translations
-translations=(
+declare -A translations=(
     ["Author_en"]="Author"
     ["Author_fr"]="Auteur"
     ["Author_de"]="Autor"
@@ -53,44 +50,34 @@ translations=(
     ["Last edited by_it"]="Ultima modifica di"
 )
 
-declare -A language_configs
-language_configs["bash"]=("Bash" "#!/bin/bash" "#")
-language_configs["python"]=("Python" "#!/usr/bin/env python" "#")
-language_configs["perl"]=("Perl" "#!/usr/bin/env perl" "#")
-language_configs["ruby"]=("Ruby" "#!/usr/bin/env ruby" "#")
-language_configs["php"]=("PHP" "#!/usr/bin/env php" "#")
-language_configs["java"]=("Java" "" "//")
-language_configs["javascript"]=("JavaScript" "" "//")
-language_configs["c"]=("C" "" "//")
-language_configs["cpp"]=("C++" "" "//")
-language_configs["go"]=("Go" "#!/usr/bin/env go" "//")
-language_configs["rust"]=("Rust" "" "//")
+# Single-string approach: "Language|Shebang|Comment"
+declare -A language_configs=(
+  [bash]="#!/bin/bash|#"
+  [python]="#!/usr/bin/env python|#"
+  [perl]="#!/usr/bin/env perl|#"
+  [ruby]="#!/usr/bin/env ruby|#"
+  [php]="#!/usr/bin/env php|#"
+  [java]="|//"
+  [javascript]="|//"
+  [c]="|//"
+  [cpp]="|//"
+  [go]="#!/usr/bin/env go|//"
+  [rust]="|//"
+)
 
-Help() {
-    cat << EOF
-Usage: $0 [options] <file>
+# Utils Functions
 
-Description:
-    This script adds a header to the specified file depending on its extension.
-
-Options:
-    -h, --help      Display this help message.
-    -v, --version   Display the version of this script.
-    -a, --author    Set the author name.
-    -l, --language  Set the language of the file.
-    -c, --comment   Set the comment character.
-    -s, --shebang   Set the shebang.
-    -t, --template  Set the template to use.
-
-Examples:
-    $0 file.py
-    $0 file.sh
-EOF
+join_by() {
+  local separator="$1"
+  shift
+  local first="$1"
+  shift
+  printf "%s" "$first" "${@/#/$separator}"
 }
 
 detect_language() {
-    local extension="${dest_file##*.}"
-    case $extension in
+    local extension="${1##*.}"
+    case "$extension" in
         sh) echo "bash" ;;
         py) echo "python" ;;
         pl) echo "perl" ;;
@@ -106,33 +93,18 @@ detect_language() {
     esac
 }
 
+# Print N copies of the comment character
 print_lines() {
-    printf -- "$1%.0s" {1..15} >> "$2"
-    echo "" >> "$2"
+    local comment_char="$1"
+    local file_path="$2"
+    printf -- "$comment_char%.0s" {1..15} >> "$file_path"
+    echo "" >> "$file_path"
 }
 
-default_header() {
-    local slang=${lang:-en}
-
-    if [[ -n "$shenamg" ]]; then
-        echo "${language_configs[$(detect_language "$file")][1]}" > "$dest_file"
-    fi
-    print_lines "$comment_character" 15 "$dest_file"
-    echo "$comment_character    $(get_translation "Author" "$slang"): $user_name" >> "$dest_file"
-    echo "$comment_character    $(get_translation "Creation Date" "$slang"): $(date)" >> "$dest_file"
-    echo "$comment_character    $(get_translation "Description" "$slang"): " >> "$dest_file"
-    print_lines "$comment_character" 15 "$dest_file"
-    echo "$comment_character    $(get_translation "Version" "$slang"): 1.0" >> "$dest_file"
-    echo "$comment_character    $(get_translation "Last edited by" "$slang"): " >> "$dest_file"
-    print_lines "$comment_character" 15 "$dest_file"
-}
-
-write_header() {
-    local file=$1
-    case $template in
-        default) default_header "$file" ;;
-        custom) custom_header "$file" ;;
-    esac
+get_translation() {
+    local key="$1"
+    local slang="$2"
+    echo "${translations["${key}_${slang}"]}"
 }
 
 contains() {
@@ -140,7 +112,7 @@ contains() {
     local seeking=$2
     local in=1
     for element in "${!array}"; do
-        if [[ $element == $seeking ]]; then
+        if [[ $element == "$seeking" ]]; then
             in=0
             break
         fi
@@ -148,8 +120,86 @@ contains() {
     return $in
 }
 
-OPTIONS=hv:a:l:c:s:t:u:
-LONGOPTS=help,version,author:,language:,comment:,shebang:,template:,update
+# Main Functions
+
+Help() {
+    cat << EOF
+Usage: $0 [options] <file>
+Description:
+    This script adds a header to the specified file depending on its extension.
+
+Options:
+    -h, --help      Display this help message.
+    -v, --version   Display the version of this script.
+    -a, --author    Set the author name.
+    -l, --language  Set the language of the file ($(join_by ", " ${VALID_LANGUAGES[*]})).
+    -c, --comment   Set the comment character ($(join_by ", " ${VALID_COMMENTS[*]})).
+    -s, --shebang   Set the shebang language ($(join_by ", " ${VALID_SHEBANGS[*]})).
+    -t, --template  Set the template to use ($(join_by ", " ${VALID_TEMPLATES[*]})).
+
+Examples:
+    $0 file.py
+    $0 file.sh
+EOF
+}
+
+
+default_header() {
+    local file="$1"
+    local slang="$2"
+
+    # Multi-line metadata
+    print_lines "$comment_character" "$file"
+    echo "$comment_character    $(get_translation "Author" "$slang"): $user_name" >> "$file"
+    echo "$comment_character    $(get_translation "Creation Date" "$slang"): $(date)" >> "$file"
+    echo "$comment_character    $(get_translation "Description" "$slang"): " >> "$file"
+    print_lines "$comment_character" "$file"
+    echo "$comment_character    $(get_translation "Version" "$slang"): 1.0" >> "$file"
+    echo "$comment_character    $(get_translation "Last edited by" "$slang"): $user_name" >> "$file"
+    print_lines "$comment_character" "$file"
+    echo "" >> "$file"
+}
+
+custom_header() {
+    local file="$1"
+    local slang="$2"
+
+    # Single-line metadata
+    # Example: # [Author: Michel] [Date: Mon Jan 21 ...] [Version: 1.0]
+    echo "${comment_character} [$(get_translation "Author" "$slang"): $user_name]" >> "$file"
+    echo "${comment_character} [$(get_translation "Creation Date" "$slang"): $(date)]" >> "$file"
+    echo "${comment_character} [$(get_translation "Description" "$slang"): ]" >> "$file"
+    echo "${comment_character} [$(get_translation "Version" "$slang"): 1.0]" >> "$file"
+    echo "${comment_character} [$(get_translation "Last edited by" "$slang"): $user_name]" >> "$file"
+}
+
+write_header() {
+    local file="$1"
+    local slang="${lang:-en}"
+
+    local detected_lang="$(detect_language "$file")"
+    local config="${language_configs[$detected_lang]}"
+    IFS='|' read -r config_shebang config_comment <<< "$config"
+
+    # Optional shebang line
+    if [[ -n "$shebang" && "$shebang" != "none" ]]; then
+        echo "$config_shebang" > "$file"
+    fi
+
+    # Use the language’s default comment if user didn’t override
+    if [[ -z "$comment_character" ]]; then
+        comment_character="$config_comment"
+    fi
+
+    case "$template" in
+        default) default_header "$file" "$slang" ;;
+        single)  custom_header "$file" "$slang";;
+    esac
+}
+
+
+OPTIONS=hva:l:c:s:t:
+LONGOPTS=help,version,author:,language:,comment:,shebang:,template:
 
 PARSED=$(getopt --options=$OPTIONS --longoptions=$LONGOPTS --name "$0" -- "$@")
 if [[ $? -ne 0 ]]; then
@@ -164,10 +214,10 @@ while true; do
             Help
             exit;;
         -v|--version)
-            echo $version
+            echo "Version $version"
             exit;;
         -a|--author)
-            username="$2"
+            user_name="$2"
             shift 2;;
         -l|--language)
             if contains VALID_LANGUAGES "$2"; then
@@ -179,7 +229,7 @@ while true; do
             shift 2;;
         -c|--comment)
             if contains VALID_COMMENTS "$2"; then
-                comment="$2"
+                comment_character="$2"
             else
                 echo "Error: Invalid comment character. Valid options are: ${VALID_COMMENTS[*]}"
                 exit 1
@@ -210,14 +260,20 @@ while true; do
     esac
 done
 
+# Final argument after options should be the filename
 if [[ -n "$1" && ! "$1" =~ ^- ]]; then
     dest_file="$1"
-    if [[ -f "$dest_file"]]; then
-        temp_header=$(mktemp)
+    # If file already exists, prepend the header
+    if [[ -f "$dest_file" ]]; then
+        extension="${dest_file##*.}"
+        temp_header=$(mktemp --suffix=".$extension")
         write_header "$temp_header"
-        sed -i "1e cat $temp_header" "$dest_file"
-        rm "$temp_header"
-        exit 0
+
+        # Insert the header at the top
+        { cat "$temp_header"; cat "$dest_file"; } > "${dest_file}.new"
+        mv "${dest_file}.new" "$dest_file"
+        rm -f "$temp_header"
+        exit 0;
     fi
     write_header "$dest_file"
 else
